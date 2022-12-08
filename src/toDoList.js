8c5c5a55e4204a2f3845495d7dda675ffdbb9c85
tanveerisonline <tanveerisonline@gmail.com>
2022-12-08 14:38:35 +0530

    Add clear all completed tasks

--- src/toDoList.js
+++ src/toDoList.js
@@ -1,6 +1,9 @@
-const listContainer = document.querySelector('.todo-list-items');
+import updateCurrentStatus from './update.js';
+
+const todoListContainer = document.querySelector('.todo-list-items');
 const todoInput = document.querySelector('.todo-input');
-const addTodoBtn = document.querySelector('#add-button');
+const addTodoListButton = document.querySelector('#add-button');
+const clearAllCompletedTaskBtn = document.querySelector('.clear-all-inputs p');
 
 export default class Todos {
   constructor(tasks = []) {
@@ -10,35 +13,57 @@ export default class Todos {
   displayList(task) {
     const list = document.createElement('li');
     list.setAttribute('dataindex', task.index);
-    list.innerHTML = `
-        <div id="task-${task.index}">
-            <input type="checkbox" name="" class="task-completed check ${task.completed ? 'hide' : ''}">
-            <i class="fa fa-check list-drag-btn check check-icon ${!task.completed ? 'hide' : ''}"></i>
-            <input type="text" class="todo-input-edit ${task.completed ? 'completed' : ''}" value="${task.description}" />
-        </div>
-      `;
-    const dragBtn = document.createElement('span');
-    dragBtn.className = 'fa fa-ellipsis-vertical list-drag-btn';
-    const trashBtn = document.createElement('span');
+    const taskContainer = document.createElement('div');
+    taskContainer.id = `task-${task.index}`;
+    const check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.className = task.completed ? 'task-complete check hide' : 'task-complete check';
+    taskContainer.appendChild(check);
+
+    const checkIconContainer = document.createElement('span');
+    checkIconContainer.className = !task.completed ? 'hide' : '';
+    const checkIcon = document.createElement('i');
+    checkIcon.className = 'fa fa-check list-drag-btn check check-icon';
+    checkIconContainer.appendChild(checkIcon);
+    taskContainer.appendChild(checkIconContainer);
+
+    const text = document.createElement('input');
+    text.setAttribute('type', 'text');
+    text.className = task.completed ? 'todo-input-edit completed' : 'todo-input-edit';
+    text.value = task.description;
+    taskContainer.appendChild(text);
+
+    list.appendChild(taskContainer);
+
+    const dragButton = document.createElement('span');
+    dragButton.className = 'list-drag-btn';
+    const dragIcon = document.createElement('i');
+    dragIcon.className = 'fa fa-ellipsis-vertical';
+    dragButton.appendChild(dragIcon);
+
+    const trashBinButton = document.createElement('span');
     const trashIcon = document.createElement('i');
-    trashBtn.className = 'list-delete-btn hide';
+    trashBinButton.className = 'list-delete-btn hide';
     trashIcon.className = 'fa fa-trash';
-    trashBtn.appendChild(trashIcon);
-    list.appendChild(dragBtn);
-    list.appendChild(trashBtn);
-    listContainer.appendChild(list);
-    list.onclick = () => {
+    trashBinButton.appendChild(trashIcon);
+    list.appendChild(dragButton);
+    list.appendChild(trashBinButton);
+    todoListContainer.appendChild(list);
+    document.onclick = () => {
       const todoInputs = document.querySelectorAll('.todo-input-edit');
       todoInputs.forEach((input) => {
         const parent = input.parentNode.parentNode;
         const drag = parent.children[1];
         const trash = parent.children[2];
+        const completedStatus = input.classList.contains('completed');
         if (input === document.activeElement) {
           drag.classList.add('hide');
           trash.classList.remove('hide');
+          input.style.textDecoration = 'none';
         } else {
           drag.classList.remove('hide');
           trash.classList.add('hide');
+          if (completedStatus) input.style.textDecoration = 'line-through';
         }
       });
     };
@@ -52,13 +77,21 @@ export default class Todos {
       }
     });
 
-    trashBtn.addEventListener('click', () => {
+    trashBinButton.addEventListener('click', () => {
       this.removeTask(task);
+    });
+
+    check.addEventListener('change', () => {
+      this.handleCompleted(task);
+    });
+
+    checkIconContainer.addEventListener('click', () => {
+      this.handleCompleted(task);
     });
   }
 
-  getTasks() {
-    listContainer.innerHTML = '';
+  getTodoTasks() {
+    todoListContainer.innerHTML = '';
     if (localStorage.getItem('todoItems')) {
       this.tasks = JSON.parse(localStorage.getItem('todoItems'));
       this.tasks.sort((a, b) => a.index - b.index);
@@ -71,10 +104,10 @@ export default class Todos {
     }
   }
 
-  addTask(task) {
+  addTodoTask(task) {
     this.tasks.push(task);
     localStorage.setItem('todoItems', JSON.stringify(this.tasks));
-    this.getTasks();
+    this.getTodoTasks();
   }
 
   removeTask(selectedTask) {
@@ -84,7 +117,7 @@ export default class Todos {
       return task;
     });
     localStorage.setItem('todoItems', JSON.stringify(this.tasks));
-    this.getTasks();
+    this.getTodoTasks();
   }
 
   editTask(selectedTask, text) {
@@ -95,15 +128,31 @@ export default class Todos {
       return task;
     });
     localStorage.setItem('todoItems', JSON.stringify(this.tasks));
-    this.getTasks();
+    this.getTodoTasks();
+  }
+
+  handleCompleted(selectedTask) {
+    updateCurrentStatus(this.tasks, selectedTask);
+    localStorage.setItem('todoItems', JSON.stringify(this.tasks));
+    this.getTodoTasks();
+  }
+
+  clearAllCompleted() {
+    this.tasks = this.tasks.filter((task) => task.completed === false);
+    this.tasks.map((task, index) => {
+      task.index = index + 1;
+      return task;
+    });
+    localStorage.setItem('todoItems', JSON.stringify(this.tasks));
+    this.getTodoTasks();
   }
 }
 
 const todos = new Todos();
 
-todos.getTasks();
+todos.getTodoTasks();
 
-addTodoBtn.addEventListener('click', (e) => {
+addTodoListButton.addEventListener('click', (e) => {
   e.preventDefault();
   const index = todos.tasks.length + 1;
   if (todoInput.value !== '') {
@@ -112,7 +161,7 @@ addTodoBtn.addEventListener('click', (e) => {
       completed: false,
       index,
     };
-    todos.addTask(newTask);
+    todos.addTodoTask(newTask);
     todoInput.value = '';
   }
 });
@@ -120,6 +169,10 @@ addTodoBtn.addEventListener('click', (e) => {
 todoInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     e.preventDefault();
-    addTodoBtn.click();
+    addTodoListButton.click();
   }
+});
+
+clearAllCompletedTaskBtn.addEventListener('click', () => {
+  todos.clearAllCompleted();
 });
